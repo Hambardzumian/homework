036d2bf9946a088214d246441bbd2d173f370f0d
Hayk Hambardzumyan <hambardzumyanhayk309@gmail.com>
2020-09-28 20:48:39 +0400

    a

---
+++ task1.py
@@ -0,0 +1,17 @@
+#print("Hello World!!!"," IM Hayk",5 , end="..")
+
+print("Name ","\t Hayk", "Surename", "\t Hambardzumyan", sep="\n" )
+
+
+
+print("Name",end="\t")
+print("Hayk",end="\t")
+print("Surename",end="\t")
+print("Hambardzumyan")
+
+
+
+#sepy- amen andami mijev exac@
+#end- toxadardz 
+
+
