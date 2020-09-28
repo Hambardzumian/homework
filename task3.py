c19f56078e686906b236e3caf79c0498809556f4
Hayk Hambardzumyan <hambardzumyanhayk309@gmail.com>
2020-09-28 20:54:27 +0400

    new commit

---
+++ task3.py
@@ -0,0 +1,2 @@
+print( " Name \n\t Hayk \n Surename\n\t Hambardzumyan \n Father \n\t Tigran Hambardzumyan \n Mother \n\t Bella Sargsyan \n State \n\t Ararat \n Region \n\t Masis \n Shool \n\t Ayntap N1 \n IT Shool\n\t Basic Centre" )
+
---
+++ task4.py
@@ -0,0 +1,3 @@
+print('Hello Python World, where "\\n"  is used for moving new line and "\\t" is used for a tab ')
+
+ 
